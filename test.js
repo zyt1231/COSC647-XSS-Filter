98709b5c058d33edd28757e3a03037593fba75e1
TangTing528 <TangTing528@users.noreply.github.com>
2016-12-15 23:49:18 -0500

    Update test.js

--- test.js
+++ test.js
@@ -23,8 +23,8 @@ var inputs = $('input,textarea')
         $t = $(this);
         if ($t.data('oldVal') !== $t.val()) {
             var result = check($t.val());
-            if (result.xss) {
-                $t.trigger('newInput', result.str);
+           if(result.length > 0) {
+                $t.trigger('newInput', result);
             }
             $t.data('oldVal', $t.val());
         }
@@ -32,68 +32,36 @@ var inputs = $('input,textarea')
     setTimeout(scan, 100);
 })();
 
-//xss type vunerability
-var XSSChar = ['&', '<', '>', '\'', '"', '/'];
-var codeXSSChar = ['&amp', '&lt', '&gt', '&quot', '&#x27', '&3x2F'];
-
-var XSSHTMLCode = {
-    a: ['target', 'href', 'title'],
-    abbr: ['title'],
-    area: ['shape', 'coords', 'href', 'alt'],
-    audio: ['autoplay', 'controls', 'loop', 'preload', 'src'],
-    blockquote: ['cite'],
-    col: ['align', 'valign', 'span', 'width'],
-    colgroup: ['align', 'valign', 'span', 'width'],
-    font: ['color', 'size', 'face'],
-    img: ['src', 'alt', 'title', 'width', 'height'],
-    ins: ['datetime'],
-    table: ['width', 'border', 'align', 'valign'],
-    tbody: ['align', 'valign'],
-    td: ['width', 'rowspan', 'colspan', 'align', 'valign'],
-    tfoot: ['align', 'valign'],
-    th: ['width', 'rowspan', 'colspan', 'align', 'valign'],
-    thead: ['align', 'valign'],
-    tr: ['rowspan', 'align', 'valign'],
-    video: ['autoplay', 'controls', 'loop', 'preload', 'src', 'height', 'width'],
-    div: ['attr']
+var blackList = {
+  script: [],
+  img: ['src', 'onmouseover', 'onerror', 'dynsrc', 'lowsrc'],
+  iframe: ['src'],
+  input: ['src'],
+  body: ['background', 'onload'],
+  sytle: ['url', 'import'],
+  svg: ['onload'],
+  link: ['rel'],
+  a: ['href'],
+  video: ['onerror'],
+  php: ['echo']
 };
 
-
-//check xss vunerability
-function check(str) {
-    var result = new Object();
-    result.str = "";
-    result.xss = false;
-    for (var i in XSSChar) {
-        if (str.includes(XSSChar[i])) {
-            result.str += '"' + XSSChar[i] + "\" in the code. It should be changed to \"" + codeXSSChar[i] + "\"<br>";
-            result.xss = true;
-        }
-    }
-
-    //console.log(result.str);
-    return result;
-};
-
-//check if the tag in string
 function CheckTags(str) {
     var string = "<div>" + str + "</div>";
-    $(function () {
+    var result = "";
         var $elements = $(string);//this turns your string into real html
-        for (var key in XSSHTMLCode) {
+        for (var key in blackList) {
             var element = $elements.find(key);
-            var attrs = XSSHTMLCode[key]
+            var attrs = blackList[key]
             if (element.length > 0) {
-                attrs.forEach(function (attr) {
-                    if (element.attr(attr)) {
-                        // console.log(element.attr(attr));
-                        var output = 'Found <' + key + '>' + " in the string with attribute " + attr + " = " + element.attr(attr) + "\n";
-                        console.log(output);
+                attrs.forEach(function(attr){
+                    if (element.attr(attr)){
+                        //console.log(element.attr(attr));
+                        result += 'XSS Vulnerability: &lt;' + key + '&gt; ' + attr + " = <br/>";
                     }
                 });
             };
         }
-    });
+    //console.log(result);
+    return result;
 }
-
-CheckTags("<a href='www.attack.com'></a><area href='www.bank.com'></areahref><thead></thead>");
